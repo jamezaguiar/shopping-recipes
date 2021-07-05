3f85aa41166ded6ba30a0c2926af27e53e3e95d4
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-07-05 08:40:08 -0300

    env: update environment variables to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  firebaseAPIKey: 'AIzaSyDb0xTaRAoxyCgvaDF3kk5VYOsTwB_3o7Y'
+  firebaseAPIKey: "AIzaSyDb0xTaRAoxyCgvaDF3kk5VYOsTwB_3o7Y",
+  firebaseRecipesURL:
+    "https://shopping-recipes-7fb45-default-rtdb.firebaseio.com/recipes.json",
 };
