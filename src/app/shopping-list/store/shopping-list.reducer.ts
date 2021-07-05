d531bf440d59f5751b1717722eb7ac95687a653d
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-07-05 12:29:40 -0300

    refact: putting reducer on store folder

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,5 +1,7 @@
 import { Action } from '@ngrx/store';
 
+import { ADD_INGREDIENT } from './shopping-list.actions';
+
 import { Ingredient } from '@shared/ingredient.model';
 
 const initialState = {
@@ -8,7 +10,7 @@ const initialState = {
 
 export function shoppingListReducer(state = initialState, action: Action) {
   switch (action.type) {
-    case 'ADD_INGREDIENT':
+    case ADD_INGREDIENT:
       return {
         ...state,
         ingredients: [...state.ingredients, action.payload],
