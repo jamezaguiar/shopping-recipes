397a260f9ea10a43987ed7c7cd006da338c6c086
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-07-18 17:17:14 -0300

    feat: update and delete actions

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -5,6 +5,8 @@ import { Ingredient } from '@shared/ingredient.model';
 export enum ShoppingListTypes {
   ADD_INGREDIENT = 'ADD_INGREDIENT',
   ADD_INGREDIENTS = 'ADD_INGREDIENTS',
+  UPDATE_INGREDIENT = 'UPDATE_INGREDIENT',
+  DELETE_INGREDIENT = 'DELETE_INGREDIENT',
 }
 
 export class AddIngredient implements Action {
@@ -19,4 +21,20 @@ export class AddIngredients implements Action {
   constructor(public payload: Ingredient[]) {}
 }
 
-export type ShoppingListActions = AddIngredient | AddIngredients;
+export class UpdateIngredient implements Action {
+  readonly type = ShoppingListTypes.UPDATE_INGREDIENT;
+
+  constructor(public payload: { index: number; newIngredient: Ingredient }) {}
+}
+
+export class DeleteIngredient implements Action {
+  readonly type = ShoppingListTypes.DELETE_INGREDIENT;
+
+  constructor(public payload: { index: number }) {}
+}
+
+export type ShoppingListActions =
+  | AddIngredient
+  | AddIngredients
+  | UpdateIngredient
+  | DeleteIngredient;
